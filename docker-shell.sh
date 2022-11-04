69e5ada3f58ec442886587d80bdbbb25e0406d7b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-05 07:47:31 +0900

    add shell script

---
+++ docker-shell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function build_docker {
+    docker build -t svn2git `pwd`/svn2git
+}
+
+function run_docker {
+    docker run --rm -it -v `pwd`/homedir:/homedir \
+    svn2git $@
+}
+
+function show_help {
+    echo $0 build
+    echo $0 bash
+    echo $0 shell
+}
+
+case $1 in
+  "build" ) build_docker ;;
+  "bash"  ) run_docker bash ;;
+  "shell" ) run_docker bash ;;
+  *       ) show_help ;;
+esac

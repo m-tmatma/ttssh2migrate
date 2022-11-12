b3c8615402de8d8c3d11cd823db159e816a717f7
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-12 10:44:20 +0900

    add ignore files

--- 2.fiter-svndmp.sh
+++ 2.fiter-svndmp.sh
@@ -4,5 +4,28 @@ DST_REPO=workdir/ttssh2
 rm -rf $DST_REPO
 svnadmin create --compatible-version 1.8 $DST_REPO
 cat workdir/ttssh2.svndmp | \
-    svndumpfilter exclude "･ｳ･ﾔ｡ｼ ｡ﾁ ttpdlg.rc" "/Attic" | \
+    svndumpfilter exclude \
+        "･ｳ･ﾔ｡ｼ ｡ﾁ ttpdlg.rc" \
+        "/Attic" \
+        TTProxy.aps \
+        TTProxy.ncb \
+        TTProxy.vcproj.SAI.yutaka.user \
+        cygterm.res \
+        ttermpro.aps \
+        ttermpro.ncb \
+        ttermpro.pch \
+        ttpcmn.pch \
+        ttpdlg.aps \
+        ttpdlg.pch \
+        ttpfile.pch \
+        ttpmacro.pch \
+        ttpmenu.ncb \
+        ttpset.pch \
+        ttptek.pch \
+        ttssh.aps \
+        ttssh.ncb \
+        ttssh.opt \
+        ttxkanjimenu.aps \
+        ttxssh.aps \
+        | \
     svnadmin load $DST_REPO

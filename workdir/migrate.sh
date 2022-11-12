9898e8b8418c95bf0ac3987c0c1d4bfd97268d81
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-12 17:46:35 +0900

    fix svn-all-fast-export path

--- workdir/migrate.sh
+++ workdir/migrate.sh
@@ -7,7 +7,7 @@ GITDIR=$SCRIPT_DIR/gitdir
 mkdir -p $GITDIR
 cd       $GITDIR
 
-/usr/local/svn2git/svn-all-fast-export \
+svn-all-fast-export \
     --rules /workdir/conf/input.rules \
     --add-metadata \
     --svn-branches \

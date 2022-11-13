6193c0f607e750b4405bc6c8c8093132427f4ff0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-13 16:39:37 +0900

    update mirror script

--- workdir/1.mirror-ttssh2.sh
+++ workdir/1.mirror-ttssh2.sh
@@ -4,12 +4,20 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 URL=file://$SCRIPT_DIR/ttssh2
 
 cd $SCRIPT_DIR
-svnadmin create ttssh2
+if [ ! -f ttssh2/format ]; then
+    svnadmin create ttssh2
+
+    cd $SCRIPT_DIR/ttssh2
+    echo '#!/bin/sh'  > hooks/pre-revprop-change
+    echo 'exit 0'    >> hooks/pre-revprop-change
+    chmod +x            hooks/pre-revprop-change
+
+    echo "Running: svnsync init"
+    svnsync init $URL http://svn.osdn.net/svnroot/ttssh2/
+else
+    echo "Skip: svnsync init"
+fi
 
 cd $SCRIPT_DIR/ttssh2
-echo '#!/bin/sh'  > hooks/pre-revprop-change
-echo 'exit 0'    >> hooks/pre-revprop-change
-chmod +x            hooks/pre-revprop-change
-
-svnsync init $URL http://svn.osdn.net/svnroot/ttssh2/
+echo "Running: svnsync sync"
 svnsync sync $URL

6c2f8515dc627af4c82899318a4f627363a0ee5c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-13 20:26:26 +0900

    use -it on shell param only

--- docker-shell.sh
+++ docker-shell.sh
@@ -5,12 +5,13 @@ function build_docker {
 }
 
 function run_docker {
+    COMMON_PARAM=$1
     mkdir -p `pwd`/workdir
     docker run --rm \
         -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) \
-        -it -v `pwd`/workdir:/home/work \
+        $COMMON_PARAM -v `pwd`/workdir:/home/work \
         -w /home/work \
-        svn2git_plus $@
+        svn2git_plus ${@:2}
 }
 
 function show_help {
@@ -22,8 +23,8 @@ function show_help {
 
 case $1 in
   "build" ) build_docker ;;
-  "bash"  ) run_docker bash ;;
-  "shell" ) run_docker bash ;;
-  "migrate" ) run_docker ./migrate.sh ;;
+  "bash"  ) run_docker -it bash ;;
+  "shell" ) run_docker -it bash ;;
+  "migrate" ) run_docker "" ./migrate.sh ;;
   *       ) show_help ;;
 esac

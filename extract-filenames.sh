0b991ed1336ff7d6abcc2124b31685377e4bea5f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-19 08:59:50 +0900

    add extention check

--- extract-filenames.sh
+++ extract-filenames.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 WORKDIR=$SCRIPT_DIR/workdir
@@ -8,3 +8,4 @@ GIT_DIR=$GIT_ROOT/ttssh2
 
 cd       $GIT_DIR
 git rev-list --all | xargs -n 1 git ls-files | xargs -n 1 basename | tee $WORKDIR/filenames.txt
+cat $WORKDIR/filenames.txt | python3 -c "import sys; import os; lines = sys.stdin.readlines(); print(''.join({os.path.splitext(os.path.basename(line))[1] for line in lines}))" | tee $WORKDIR/extentions.txt

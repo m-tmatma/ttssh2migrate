85ef2a086dfee1799831ca51eaae2dfdb4d8da47
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-19 10:10:54 +0900

    sort full path

--- extract-filenames.sh
+++ extract-filenames.sh
@@ -8,5 +8,6 @@ GIT_DIR=$GIT_ROOT/ttssh2
 
 cd       $GIT_DIR
 git rev-list --all | xargs -n 1 git ls-tree -r --name-only | $SCRIPT_DIR/drop-duplicate.py | tee $WORKDIR/file-full-names.txt
+sort $WORKDIR/file-full-names.txt > $WORKDIR/file-sorted-full-names.txt
 cat $WORKDIR/file-full-names.txt | xargs -n 1 basename | sort | uniq | tee $WORKDIR/file-names.txt
 cat $WORKDIR/file-names.txt | python3 -c "import sys; import os; lines = sys.stdin.readlines(); print(''.join({os.path.splitext(os.path.basename(line))[1] for line in lines}))" | tee $WORKDIR/file-ext.txt

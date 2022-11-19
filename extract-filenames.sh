ceb683a213756239f5600e6742b0a2ae74df2b9a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-19 09:26:52 +0900

    print removing duplicate

--- extract-filenames.sh
+++ extract-filenames.sh
@@ -7,5 +7,5 @@ GIT_ROOT=$WORKDIR/gitdir
 GIT_DIR=$GIT_ROOT/ttssh2
 
 cd       $GIT_DIR
-git rev-list --all | xargs -n 1 git ls-files | xargs -n 1 basename | tee $WORKDIR/filenames.txt
-cat $WORKDIR/filenames.txt | python3 -c "import sys; import os; lines = sys.stdin.readlines(); print(''.join({os.path.splitext(os.path.basename(line))[1] for line in lines}))" | tee $WORKDIR/extentions.txt
+git rev-list --all | xargs -n 1 git ls-tree -r --name-only | $SCRIPT_DIR/drop-duplicate.py | tee $WORKDIR/file-names.txt
+cat $WORKDIR/file-names.txt | python3 -c "import sys; import os; lines = sys.stdin.readlines(); print(''.join({os.path.splitext(os.path.basename(line))[1] for line in lines}))" | tee $WORKDIR/file-ext.txt

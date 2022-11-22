dd9b6bca39649db04e67863db23360ba8778811f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-22 23:26:16 +0900

    define sample data

--- make-identity-map.py
+++ make-identity-map.py
@@ -17,9 +17,8 @@ with open(input, "r") as f_in, open(output, "w") as f_out:
         user = row["user"]
         email = row["email"]
 
-        # janesvnaccountname = Jane Doe <jane.doe@example.com>
-        if email == "":
-            email = f"{user}@example.com"
+        # example:
+        #   janesvnaccountname = Jane Doe <jane.doe@example.com>
         data = f"{user} = {user} <{email}>"
         #print(data)
         f_out.write(data + "\n")

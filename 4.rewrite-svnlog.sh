3f95bb90197b64ccbf4bb13915b39c296e5273f6
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-23 12:39:39 +0900

    save each revlog

--- 4.rewrite-svnlog.sh
+++ 4.rewrite-svnlog.sh
@@ -6,23 +6,31 @@ cd $SCRIPT_DIR
 WORKDIR=$SCRIPT_DIR/workdir
 SRC_REPO=$WORKDIR/ttssh2.org
 DST_REPO=$WORKDIR/ttssh2
+SRC_LOG_DIR=$WORKDIR/ttssh2.srclog
+DST_LOG_DIR=$WORKDIR/ttssh2.dstlog
+
+rm   -rf $SRC_LOG_DIR $DST_LOG_DIR
+mkdir -p $SRC_LOG_DIR
+mkdir -p $DST_LOG_DIR
 
 YOUNGEST=$(svnlook youngest $DST_REPO)
 echo $YOUNGEST
 for rev in `seq 1 $YOUNGEST`
 do
-    #echo processing $rev
-    svnlook log -r $rev $DST_REPO > $WORKDIR/revlog-org.txt
-    cat  $WORKDIR/revlog-org.txt | \
-        python3 -c 'import sys,re; [print(re.sub(r"\br(\d+)\b",r"https://osdn.net/projects/ttssh2/scm/svn/commits/\1",l),end="") for l in sys.stdin]' | \
-        python3 -c 'import sys,re; [print(re.sub(r"\b#(\d+)\b",r"https://osdn.net/projects/ttssh2/ticket/\1"         ,l),end="") for l in sys.stdin]' > $WORKDIR/revlog-new.txt
+    SRC_LOG=$SRC_LOG_DIR/r$rev.log
+    DST_LOG=$DST_LOG_DIR/r$rev.log
 
-    diff $WORKDIR/revlog-org.txt $WORKDIR/revlog-new.txt
+    #echo processing $rev
+    svnlook log -r $rev $DST_REPO > $SRC_LOG
+    cat $SRC_LOG | \
+        python3 -c 'import sys,re; [print(re.sub(r"\br(\d+)\b",r"https://osdn.net/projects/ttssh2/scm/svn/commits/\1",l),end="") for l in sys.stdin]' | \
+        python3 -c 'import sys,re; [print(re.sub(r"\b#(\d+)\b",r"https://osdn.net/projects/ttssh2/ticket/\1"         ,l),end="") for l in sys.stdin]' > $DST_LOG
+
+    diff $SRC_LOG $DST_LOG
     if [ $? -ne 0 ]; then
         echo replacing log for $rev
-        svnadmin setlog --bypass-hooks $DST_REPO -r $rev $WORKDIR/revlog-new.txt
+        svnadmin setlog --bypass-hooks $DST_REPO -r $rev $DST_LOG
     fi
 done
-rm -f $WORKDIR/revlog-org.txt $WORKDIR/revlog-new.txt
 
 svn log -v file://$DST_REPO > $WORKDIR/svn-new-rewrite.log

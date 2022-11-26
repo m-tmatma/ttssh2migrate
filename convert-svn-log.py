056775fff57b7f62c3a0dbe691eee33485b3c686
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-26 17:52:24 +0900

    convert issues.

--- convert-svn-log.py
+++ convert-svn-log.py
@@ -3,6 +3,7 @@ import sys
 import re
 
 allRevs = set()
+allIssues = set()
 for line in sys.stdin:
     r = re.finditer(r"(r(\d+)\s*-\s*r(\d+))|(r(\d+)((?=[^0-9-&/=@_])|$))", line)
     for m in r:
@@ -12,9 +13,21 @@ for line in sys.stdin:
             allRevs.add(rev.replace("r", ""))
     print(line.rstrip("\r").rstrip("\n"))
 
+    r = re.finditer(r"(#(\d+))((?=[^0-9-&/=@_])|$)", line)
+    for m in r:
+        issue = m.group(0)
+        allIssues.add(issue.replace("#", ""))
+
 if allRevs:
     # print empty line for paragraph.
     print("")
     print("Revisions:")
     for rev in sorted(list(allRevs)):
         print(f"* https://osdn.net/projects/ttssh2/scm/svn/commits/{rev}")
+
+if allIssues:
+    # print empty line for paragraph.
+    print("")
+    print("Issues:")
+    for issue in sorted(list(allIssues)):
+        print(f"* https://osdn.net/projects/ttssh2/ticket/{issue}")

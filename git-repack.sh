7b7d471403514ddbae0626103641e878815b0440
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-27 16:51:25 +0900

    check repack result

--- git-repack.sh
+++ git-repack.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 WORKDIR=$SCRIPT_DIR/workdir
@@ -10,6 +10,7 @@ mkdir -p $GIT_ROOT_REPACK
 
 du -h $GIT_ROOT/ttssh2
 
+# prepare working git repository
 cp   -a $GIT_ROOT/ttssh2 $GIT_ROOT_REPACK/
 
 # See https://techbase.kde.org/Projects/MoveToGit/UsingSvn2Git for `git repack`
@@ -17,5 +18,12 @@ git  -C $GIT_ROOT_REPACK/ttssh2 repack -a -d -f --window=250 --depth=250
 
 du -h $GIT_ROOT_REPACK/ttssh2
 
+# compare original repository and repacked repository
 diff -r $GIT_ROOT/ttssh2 $GIT_ROOT_REPACK/ttssh2 || echo "" > /dev/null
 ls -lh  $GIT_ROOT/ttssh2/objects/pack $GIT_ROOT_REPACK/ttssh2/objects/pack
+
+# list up all revisoins
+git -C  $GIT_ROOT/ttssh2         rev-list --all > $WORKDIR/rev-all-org.txt
+git -C  $GIT_ROOT_REPACK/ttssh2  rev-list --all > $WORKDIR/rev-all-new.txt
+
+diff  $WORKDIR/rev-all-org.txt $WORKDIR/rev-all-new.txt

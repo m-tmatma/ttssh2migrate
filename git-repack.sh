379aaf76ff637be7d1251d67121cd8c1b7e83cb9
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-27 16:25:31 +0900

    git repack.

---
+++ git-repack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+WORKDIR=$SCRIPT_DIR/workdir
+GIT_ROOT=$WORKDIR/gitdir
+GIT_ROOT_REPACK=$WORKDIR/gitdir-repack
+
+rm   -rf $GIT_ROOT_REPACK
+mkdir -p $GIT_ROOT_REPACK
+
+du -h $GIT_ROOT/ttssh2
+
+cp   -a $GIT_ROOT/ttssh2 $GIT_ROOT_REPACK/
+git  -C $GIT_ROOT_REPACK/ttssh2 repack -a -d -f --window=250 --depth=250
+
+du -h $GIT_ROOT_REPACK/ttssh2
+
+diff -r $GIT_ROOT/ttssh2 $GIT_ROOT_REPACK/ttssh2
+ls -lh  $GIT_ROOT/ttssh2/objects/pack $GIT_ROOT_REPACK/ttssh2/objects/pack

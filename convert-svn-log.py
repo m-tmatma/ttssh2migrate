9944fec5804ff14d968b4b1d24c7c001ba57aa9c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-25 22:01:10 +0900

    check result

--- convert-svn-log.py
+++ convert-svn-log.py
@@ -87,7 +87,8 @@ if allRevs:
             cmd = ["git", "-C", repoDir, "log", "--grep", f"revision={rev}$", "--format=%H"]
             result = subprocess.check_output(cmd)
             commitHash = result.decode()
-            print(f"* r{rev}: https://github.com/{nameWithOwner}/commit/{commitHash}")
+            if commitHash != "":
+                print(f"* r{rev}: https://github.com/{nameWithOwner}/commit/{commitHash}")
         except Exception as e:
             print(f"* r{rev}:")
             print(e)

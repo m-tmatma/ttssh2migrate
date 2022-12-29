c413b97f7d8eb861fe939e09c91f21c9ae4b8563
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-29 15:45:49 +0900

    checkout after commit

--- 2-2.svn-eol-style.sh
+++ 2-2.svn-eol-style.sh
@@ -13,8 +13,9 @@ SRC_REPO=$WORKDIR/ttssh2.filter
 DST_REPO=$WORKDIR/ttssh2
 
 cp -a $SRC_REPO $DST_REPO
-svn co file://$DST_REPO/trunk             $WORKDIR/svnwork/trunk
-svn co file://$DST_REPO/branches/4-stable $WORKDIR/svnwork/4-stable
 
+svn co file://$DST_REPO/trunk             $WORKDIR/svnwork/trunk
 (cd $WORKDIR/svnwork/trunk    && find -name "*.c" -or -name "*.h" -or  -name "*.cpp" | xargs svn propset svn:eol-style native && svn commit -m "svn:eol-style native")
+
+svn co file://$DST_REPO/branches/4-stable $WORKDIR/svnwork/4-stable
 (cd $WORKDIR/svnwork/4-stable && find -name "*.c" -or -name "*.h" -or  -name "*.cpp" | xargs svn propset svn:eol-style native && svn commit -m "svn:eol-style native")

1c98742d8deef5f3481bf79e037a5bba4b36d70d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-29 15:30:38 +0900

    set svn:eol-style on trunk, 4-stable

--- 2-2.svn-eol-style.sh
+++ 2-2.svn-eol-style.sh
@@ -10,7 +10,8 @@ fi
 
 WORKDIR=$SCRIPT_DIR/workdir
 DST_REPO=$WORKDIR/ttssh2
-svn co file://$DST_REPO $WORKDIR/svnwork
-cd $WORKDIR/svnwork
-find -name "*.c" -or -name "*.h" -or  -name "*.cpp" | xargs svn propset svn:eol-style native
-svn commit -m "svn:eol-style native"
+svn co file://$DST_REPO/trunk             $WORKDIR/svnwork/trunk
+svn co file://$DST_REPO/branches/4-stable $WORKDIR/svnwork/4-stable
+
+(cd $WORKDIR/svnwork/trunk    && find -name "*.c" -or -name "*.h" -or  -name "*.cpp" | xargs svn propset svn:eol-style native && svn commit -m "svn:eol-style native")
+(cd $WORKDIR/svnwork/4-stable && find -name "*.c" -or -name "*.h" -or  -name "*.cpp" | xargs svn propset svn:eol-style native && svn commit -m "svn:eol-style native")

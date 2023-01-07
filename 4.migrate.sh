dd004378b24a5e7a0d9efe9b8287b28c45020e1e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-07 14:27:05 +0900

    show log path

--- 4.migrate.sh
+++ 4.migrate.sh
@@ -30,3 +30,4 @@ $SCRIPT_DIR/svn2git/svn-all-fast-export \
 echo rename defaut branch to main
 git -C $GIT_ROOT/ttssh2 branch -m main
 git -C $GIT_ROOT/ttssh2 log --all --grep=NotFound > $GIT_ROOT/log-NotFound.log
+echo see $SCRIPT_DIR/workdir/gitdir/convert-svn-log.log

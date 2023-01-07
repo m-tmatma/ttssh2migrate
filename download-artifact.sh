dd31d972d09871d61dfbf8bab2ada7d009103261
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-07 10:53:28 +0900

    use databaseId

--- download-artifact.sh
+++ download-artifact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
 gh run list -L 1 -b main
-gh run download $(gh run list -L 1 -b main --json url  --jq '.[].url' | xargs -n1 basename) $@
+gh run download $(gh run list -L 1 -b main --json databaseId  --jq '.[].databaseId') $@

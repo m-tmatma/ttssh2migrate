d8df51ba62767e7a5ee75385e3d257e42731035b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-08 14:08:47 +0900

    specifiy branch

--- download-artifact.sh
+++ download-artifact.sh
@@ -1,4 +1,26 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-gh run list -L 1 -b main
-gh run download $(gh run list -L 1 -b main --json databaseId  --jq '.[].databaseId') $@
+function show_help () {
+    echo $0 latest [...]
+    echo $0 main   [...]
+    echo $0 help
+}
+
+ARG=$1
+BRANCH_PARAM=
+if  [ x"$ARG" = x"help" ]; then
+    show_help
+    exit 0
+elif [ x"$ARG" = x"latest" ]; then
+    BRANCH_PARAM=
+    shift
+elif [ x"$ARG" = x"main" ]; then
+    BRANCH_PARAM="-b main"
+    shift
+else
+    BRANCH_PARAM="-b main"
+fi
+
+set -x
+gh run list -L 1 $BRANCH_PARAM
+gh run download $(gh run list -L 1 $BRANCH_PARAM --json databaseId  --jq '.[].databaseId') $@
